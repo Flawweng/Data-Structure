bb88164e1c0e5c4bfcf234ed3da4319e0249e802
Flawweng <frederic.lawwengsam@gmail.com>
2020-04-19 01:03:49 +0200

    Enhancement

--- LinkedList.py
+++ LinkedList.py
@@ -6,7 +6,9 @@ class LinkedList:
         for arg in args:
             self.add(arg)
 
-    # Add at the last element of the list, a new node with the value passed as argument.
+    """
+    Add at the end of the list a new node with the value passed.
+    """
     def add(self, value):
         # List is empty
         if self.head is None:
@@ -20,36 +22,44 @@ class LinkedList:
         self.size += 1
         return self
 
-    # Add the element at index
+    """
+    Add a node at the index with the value passed.
+    """
     def addAtIndex(self, value, index):
-        # No insert if index superior to 
+        # No insert if index superior to. 
         if(self.size - 1 < index):
             raise ValueError("Cannot insert node at index " + str(index) + " - list size: " + str(self.size))
         currentNode = self.head
-        i = 0
-        while i < index -1:
+        # Loop until to be at node before the one to added.
+        for i in range(index - 1):
             currentNode = currentNode.next
             i += 1
         currentNode.next = self.Node(value, currentNode.next)
         self.size += 1
+        return self
 
-    # Remove element at index
+   """
+   Remove from the list the node at the index.
+   """ 
     def removeAtIndex(self, index):
         if(self.size - 1 < index):
             raise ValueError("Cannot remove node at index " + str(index) + " - list size: " + str(self.size))
-        currentNode = self.head
         if index == 0:
-            self.head = currentNode.next
+            self.head = self.head.next
         else:
-            i = 0
-            while i < index -1:
+            currentNode = self.head
+            # Loop until to be at node before the one to be deleted.
+            for i in range(index - 1):
+                print(i)
                 currentNode = currentNode.next
-                i += 1
             currentNode.next = currentNode.next.next if currentNode.next.next else None
         self.size -= 1
+        return self
 
 
-    # Remove from the list all nodes with the value passed as argument
+    """
+    Remove from the list all nodes with the value passed.
+    """ 
     def remove(self,value):
         # Test if list is empty
         if(self.head is not None):
@@ -80,7 +90,9 @@ class LinkedList:
                 currentNode = currentNode.next
         return self
 
-    # Return nodes list with the value passed as parameter
+    """
+    Search and return nodes existing in the list with the value passed.
+    """ 
     def search(self, value):
         result = []
         currentNode = self.head
@@ -89,6 +101,19 @@ class LinkedList:
                 result.append(currentNode)
             currentNode = currentNode.next
         return result
+    """
+    Test if lists are equals compared to elements values. 
+    """ 
+    def equals(self, obj):
+        if not isinstance(obj, LinkedList) or self.size != obj.size:
+            return False
+        currentNode = self.head
+        currentObjNode = obj.head
+        for i in range(self.size):
+            if currentNode.value != currentObjNode.value: return False
+            currentNode = currentNode.next
+            currentObjNode = currentObjNode.next
+        return True
                 
     def __str__(self):
         if self.head is None:
@@ -107,8 +132,15 @@ class LinkedList:
         def __str__(self):
             return self.toString()
 
+if __name__ =="__main__":
+    l = LinkedList(1, 2, "LOL", "Yes", 2) 
+    m = LinkedList(1, 2, "LOL", "Yes", 22)
+    print(l.equals(m))
 
 
+
+
+"""
 l = LinkedList(1, 2, "LOL", "Yes", 2)
 print(str(l) + "- Size: " + str(l.size))
 print("Remove element : 1")
@@ -131,8 +163,8 @@ print("Search for element 2: " + " ".join(str(node) for node in l.search(2)))
 
 l.addAtIndex("Test", 4)
 print("Add at index 4 : " + str(l) + " - Size: " + str(l.size))
-
+"""
 l = LinkedList(1, 2, "LOL", "Yes", 2)
+print(l)
 l.removeAtIndex(2)
-print("Remove element at index 2: " + str(l) + " - Size: " + str(l.size))
-
+print("Remove element at index 0: " + str(l) + " - Size: " + str(l.size))
